b9f60b214f6d31795f9c3a30b9d8877c095242ce
IguanaAzul <ramon.griffo98@gmail.com>
2021-06-18 20:36:01 -0300

    Exercício 1

--- exercicio1.js
+++ exercicio1.js
@@ -1,2 +1,13 @@
-// Faça o exercício da equação de GRAVITAÇÃO UNIVERSAL aqui
-// Este arquivo AINDA NÃO ESTÁ INCLUÍDO no arquivo HTML
+function forca_gravitacional(constante_gravitacional, massa_1, massa_2, distancia) {
+    return (constante_gravitacional * massa_1 * massa_2)/(distancia ** 2);
+}
+
+function clica_calcular(){
+    const constante_gravitacional = document.querySelector('#constante').value;
+    const massa_1 = document.querySelector('#massa1').value;
+    const massa_2 = document.querySelector('#massa2').value;
+    const distancia = document.querySelector('#distancia').value;
+    document.querySelector('#resultado').value = forca_gravitacional(constante_gravitacional, massa_1, massa_2, distancia);
+}
+
+document.querySelector('#calcular').addEventListener('click', clica_calcular);
