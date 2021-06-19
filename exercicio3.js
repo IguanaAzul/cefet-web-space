cd9d3df8496ca00977780dbb7726078d79564c7b
IguanaAzul <ramon.griffo98@gmail.com>
2021-06-18 21:24:07 -0300

    Exercício 3

--- exercicio3.js
+++ exercicio3.js
@@ -1,2 +1,13 @@
-// Faça o exercício dos PARÁGRAFOS aqui
-// Este arquivo AINDA NÃO ESTÁ INCLUÍDO no arquivo HTML
+// Fiquei um tempão tentando usardocument.querySelectorAll('.botao-expandir-retrair')
+// Mas não soube como acessar oo paragrafo  na hora de dar o toggle, aí pensei na
+// possibilidade de iterar nos próprios parágrafos.
+
+let paragrafos = document.querySelectorAll('#aliens p')
+
+for (let p of paragrafos) {
+    let botao = p.querySelector('.botao-expandir-retrair');
+    botao.addEventListener('click', function() {
+        p.classList.toggle('expandido');
+        botao.innerHTML = botao.innerHTML === '-' ? '+' : '-';
+    });
+}
