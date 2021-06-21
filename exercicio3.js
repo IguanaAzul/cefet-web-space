971f4991a33716d3fb6b877024c965513627b233
IguanaAzul <ramon.griffo98@gmail.com>
2021-06-21 13:25:07 -0300

    Reimplementação do exercício 3

--- exercicio3.js
+++ exercicio3.js
@@ -2,7 +2,7 @@ let botoes = document.querySelectorAll('.botao-expandir-retrair')
 
 for (let b of botoes) {
     b.addEventListener('click', function() {
-        p = b.parentElement;
+        let p = b.parentElement;
         p.classList.toggle('expandido');
         b.innerHTML = b.innerHTML === '-' ? '+' : '-';
     });
