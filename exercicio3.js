8a423ea0f2a7b0d10e4c4adea87ef0f98f74b41a
IguanaAzul <ramon.griffo98@gmail.com>
2021-06-21 13:24:48 -0300

    Reimplementação do exercício 3

--- exercicio3.js
+++ exercicio3.js
@@ -1,13 +1,9 @@
-// Fiquei um tempão tentando usardocument.querySelectorAll('.botao-expandir-retrair')
-// Mas não soube como acessar oo paragrafo  na hora de dar o toggle, aí pensei na
-// possibilidade de iterar nos próprios parágrafos.
+let botoes = document.querySelectorAll('.botao-expandir-retrair')
 
-let paragrafos = document.querySelectorAll('#aliens p')
-
-for (let p of paragrafos) {
-    let botao = p.querySelector('.botao-expandir-retrair');
-    botao.addEventListener('click', function() {
+for (let b of botoes) {
+    b.addEventListener('click', function() {
+        p = b.parentElement;
         p.classList.toggle('expandido');
-        botao.innerHTML = botao.innerHTML === '-' ? '+' : '-';
+        b.innerHTML = b.innerHTML === '-' ? '+' : '-';
     });
 }
